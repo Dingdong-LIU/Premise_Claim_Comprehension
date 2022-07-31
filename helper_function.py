73770cdf5685d705c776e9659fc8d943f54c6e81
Dingdong-LIU <dliuak@connect.ust.hk>
2022-07-31 11:13:40 +0800

    add: algorithm that finds center premises.

--- helper_function.py
+++ helper_function.py
@@ -25,12 +25,48 @@ def get_claims(x, return_list_of_strings=True):
     Returns:
         list: a list of strings or a list of dict object (return_list=True)
     """
-    claim_object_list = []
-    for i in x['answers']:
-        if i and i['claim'] != '': # filter out empty objects
-            claim_object_list.append(i['claim'])
-    if return_list_of_strings:
-        return [m['content'] for n in claim_object_list for m in n]
-    return claim_object_list
+    # claim_object_list = []
+    # for i in x['answers']:
+    #     if i and i['claim'] != '': # filter out empty objects
+    #         claim_object_list.append(i['claim'])
+    # if return_list_of_strings:
+    #     return [m['content'] for n in claim_object_list for m in n]
+    # return claim_object_list
+    return get_objects(x, return_list_of_string=return_list_of_strings, objects_type="claim")
 
+
+def get_premises(x, return_list_of_strings=True):
+    """Get premises from Json file as described in Quora pag
+
+    Args:
+        x (dict): Json file load by json package. Json should have the following format:
+        'question' --> str
+        'answers' --> <list of dicts> --> 'premise' --> <list of dicts>
+        return_list_of_strings (bool, optional): If True, return a list of claims. If False, return a list of dict objects. Defaults to True.
+    """
+    return get_objects(x, return_list_of_string=return_list_of_strings, objects_type='premise')
+    # premises_object_list = []
+    # for i in x['answers']:
+    #     if i:
+    #         premises_object_list.append(i['premise'])
+    # if return_list_of_strings:
+    #     return [m['content'] for n in premises_object_list for m in n]
+    # return premises_object_list
+
+def get_objects(x, return_list_of_string=True, objects_type="claim"):
+    """Retrive given objects from the Json file downloaded from Quora page
+
+    Args:
+        x (dict): Json file loaded by json package.
+        return_list_of_string (bool, optional): If True, return a list of string; else, return a list of dict objects. Defaults to True.
+        objects_type (str, optional): Should be "claim" or "premises". The type of string to extract. Defaults to "claim".
+    """
+    assert objects_type in ['claim', 'premise'], f"Your choise '{objects_type}' is not one of 'claim' or 'premise' "
+    object_list = []
+    for i in x['answers']:
+        if i:
+            object_list.append(i[objects_type])
+    if return_list_of_string:
+        return [m['content'] for n in object_list for m in n]
+    return object_list
 
